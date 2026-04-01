5e95dc6f16fecaf3a3505577c15c5ecf8063a526
Matías Roy Rodriguez <mroy@makingsense.com>
2026-04-01 12:14:30 -0300

    feat: add dt registered structure

--- pages/digital-trends/pre/digital-trends-registrado.php
+++ pages/digital-trends/pre/digital-trends-registrado.php
@@ -1,7 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/cacheSettings.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/components/modal/modal.php');
 $isPost = false;
 ?>
 
@@ -26,43 +25,54 @@ $isPost = false;
 </head>
 
 <body>
-  <div class="register-form__container hidden--vip">
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/hello-bar.php'); ?>
-  </div>
-
   <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/gtm.php'); ?>
   <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar-reg.php') ?>
   <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/share.php') ?>
   <main>
-    <div class="show--vip">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/hello-vip-module.php') ?>
-    </div>
-    <div class="hidden--vip">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/hello-module.php') ?>
-    </div>
-    <div class="gold-schedule">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/schedule/schedule.php') ?>
-    </div>
-    <div class="hidden--vip">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/entry-plans.php') ?>
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/video-ticketing.php') ?>
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/vip-features.php') ?>
-    </div>
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/referral.php') ?>
+        <?php
+    $digitalTrendsVipGridTitle = 'EMMS 2026: anticípate al futuro del Marketing';
+    $digitalTrendsVipGridColumns = 2;
+    $digitalTrendsVipGridItems = [
+      [
+        'img' => '/src/img/grid-event-types/ia.png',
+        'alt' => 'Inteligencia Artificial',
+        'title' => 'Inteligencia Artificial',
+        'text' => 'Descubre cómo usar IA para automatizar Campañas, personalizar experiencias y mejorar tus resultados.',
+      ],
+      [
+        'img' => '/src/img/grid-event-types/marketingyecommerce.png',
+        'alt' => 'Marketing y E-commerce',
+        'title' => 'Marketing y E-commerce',
+        'text' => 'Aprende estrategias para atraer tráfico, convertir clientes y fidelizar audiencias en mercados cada vez más competitivos.',
+      ],
+      [
+        'img' => '/src/img/grid-event-types/casosdeexito.png',
+        'alt' => 'Casos de Éxito',
+        'title' => 'Casos de Éxito',
+        'text' => 'Conoce a las empresas que están cambiando las reglas del juego y replica sus tácticas en 
+tu negocio.',
+      ],
+      [
+        'img' => '/src/img/grid-event-types/regalos.png',
+        'alt' => 'Sorpresas por Aniversario',
+        'title' => 'Sorpresas por Aniversario',
+        'text' => 'Descuentos exclusivos, premios y beneficios únicos por los 20 años Doppler',
+      ],
+    ];
+    ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/digital-trends/hello-module.php') ?>
 
-    <div class="show--vip">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/premium-content.php') ?>
-    </div>
-    <div class="hidden--vip">
-      <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/premium-content.php') ?>
-    </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/referral.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/premium-content.php') ?>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/academyBanner.php'); ?>
+    <?php
+    $gridTitle = $digitalTrendsVipGridTitle;
+    $gridColumns = $digitalTrendsVipGridColumns;
+    $gridItems = $digitalTrendsVipGridItems;
+    include($_SERVER['DOCUMENT_ROOT'] . '/components/digital-trends/pre/grid-event-types.php');
+    ?>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/sponsorsList.php') ?>
   </main>
-  <?php
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/components/modal/extraDataCaptor.php');
-  render_modal('modalVip', 'vipmodal',  'vip', true);
-  ?>
   <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php'); ?>
   <script src="src/<?= VERSION ?>/js/newDate.js" type="module"></script>
 

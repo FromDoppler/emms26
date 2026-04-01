0ca4592b33bb0dca95ebe1aadc06eca045c3c196
Matías Roy Rodriguez <mroy@makingsense.com>
2026-04-01 11:16:52 -0300

    feat: add referal module

--- components/referral.php
+++ components/referral.php
@@ -1,24 +1,22 @@
 <section class="referral" id="referidos">
   <div class="referral__image-people">
-    <img src="/src/img/referral/referral.png" alt="Persona promocionando su premio" />
+    <img src="/src/img/referral/referral.png" alt="Persona sosteniendo un meg&aacute;fono y un regalo" />
   </div>
 
   <div class="referral__container">
     <div class="referral__content">
-      <h2 class="referral__title">¡Recomienda el evento y gana!</h2>
+      <h2 class="referral__title">&iexcl;Recomienda el evento y gana!</h2>
 
       <p class="referral__description">
-        Súmate al <strong>PROGRAMA DE REFERIDOS</strong>: por cada persona que invites, acumulas chances de ganar gift cards y elegir tu premio.
+        S&uacute;mate al <strong>PROGRAMA DE REFERIDOS</strong>: por cada persona que invites, acumulas chances de ganar gift cards y elegir tu premio.
       </p>
-      <img src="/src/img/referral/premios.png" alt="Increibles premios" class="premios-desktop">
-      <img src="/src/img/referral/premios-mobile.png" alt="Increibles premios" class="premios-mobile">
 
-      <p class="referral__subtitle"><strong>¿Cómo participar?</strong></p>
+      <p class="referral__subtitle">&iquest;C&oacute;mo participar?</p>
       <p class="referral__instructions">
-        Comparte tu link único en Redes Sociales invitando a tu comunidad a que se registre gratis al evento. ¡Empieza ahora y suma chances de ganar!
+        Comparte tu link &uacute;nico en Redes Sociales invitando a tu comunidad a que se registre gratis al evento. Las personas que acumulen m&aacute;s referidos, &iexcl;ganan!
       </p>
+
       <?php include($_SERVER['DOCUMENT_ROOT'] . '/components/referral-button.php') ?>
     </div>
   </div>
-
 </section>

49ff421ca37bbf3596114d763ec0b8ed5738a679
MatiasRoy-Rodriguez <75322768+MatiasRoy-Rodriguez@users.noreply.github.com>
2026-04-07 12:25:18 -0300

    feat: add seo content (#26)

--- components/digital-trends/pre/home/head.php
+++ components/digital-trends/pre/home/head.php
@@ -1,35 +1,35 @@
 <?php if ($isRegistered) { ?>
-  <title>Llega el EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital</title>
+  <title>Llega el EMMS 2026: el evento online de Marketing y Comercio Electrónico.</title>
 
-  <meta name="title" content="Llega el EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta name="description" content="¡Cuenta regresiva para el evento de Marketing Digital más esperado del año! Prepárate para capacitarte e inspirarte con referentes de la industria">
+  <meta name="title" content="Llega el EMMS 2026: el evento online de Marketing y Comercio Electrónico.">
+  <meta name="description" content="¡Cuenta regresiva para el evento más esperado del año! Prepárate para capacitarte con referentes de la industria.">
 
   <meta property="og:url" content="https://goemms.com/">
-  <meta property="og:title" content="Llega el EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta property="og:description" content="¡Cuenta regresiva para el evento de Marketing Digital más esperado del año! Prepárate para capacitarte e inspirarte con referentes de la industria">
+  <meta property="og:title" content="Llega el EMMS 2026: el evento online de Marketing y Comercio Electrónico.">
+  <meta property="og:description" content="¡Cuenta regresiva para el evento más esperado del año! Prepárate para capacitarte con referentes de la industria.">
   <meta property="og:image" content="https://goemms.com/share-2025-dt.png">
   <meta name="twitter:image" content="https://goemms.com/share-2025-dt.png">
 
-  <meta property="og:site_name" content="Llega el EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta name="twitter:image:alt" content="Llega el EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
+  <meta property="og:site_name" content="Llega el EMMS 2026: el evento online de Marketing y Comercio Electrónico.">
+  <meta name="twitter:image:alt" content="Llega el EMMS 2026: el evento online de Marketing y Comercio Electrónico.">
 
   <link rel="canonical" href="https://goemms.com/" />
   <link hreflang="x-default" href="https://goemms.com/" rel="alternate" />
   <link hreflang="es-ar" href="https://goemms.com/" rel="alternate" />
 <?php } else { ?>
-  <title>Regístrate gratis al EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital</title>
+  <title>Regístrate gratis al EMMS 2026, el evento online de Marketing y Comercio Electrónico.</title>
 
-  <meta name="title" content="Regístrate gratis al EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta name="description" content="Inscríbete gratis ahora y aprende en un solo lugar todas las tendencias del Marketing Digital">
+  <meta name="title" content="Regístrate gratis al EMMS 2026, el evento online de Marketing y Comercio Electrónico.">
+  <meta name="description" content="Inscríbete gratis y accede en un solo lugar a todas las tendencias del Marketing Digital y E-commerce.">
 
   <meta property="og:url" content="https://goemms.com/">
-  <meta property="og:title" content="Regístrate gratis al EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta property="og:description" content="Inscríbete gratis ahora y aprende en un solo lugar todas las tendencias del Marketing Digital">
+  <meta property="og:title" content="Regístrate gratis al EMMS 2026, el evento online de Marketing y Comercio Electrónico.">
+  <meta property="og:description" content="Inscríbete gratis y accede en un solo lugar a todas las tendencias del Marketing Digital y E-commerce.">
   <meta property="og:image" content="https://goemms.com/share-2025-dt.png">
   <meta name="twitter:image" content="https://goemms.com/share-2025-dt.png">
 
-  <meta property="og:site_name" content="Regístrate gratis al EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
-  <meta name="twitter:image:alt" content="Regístrate gratis al EMMS Digital Trends 2025: Conferencias online y gratuitas sobre Marketing Digital">
+  <meta property="og:site_name" content="Regístrate gratis al EMMS 2026, el evento online de Marketing y Comercio Electrónico.">
+  <meta name="twitter:image:alt" content="Regístrate gratis al EMMS 2026, el evento online de Marketing y Comercio Electrónico.">
 
   <link rel="canonical" href="https://goemms.com/" />
   <link hreflang="x-default" href="https://goemms.com/" rel="alternate" />

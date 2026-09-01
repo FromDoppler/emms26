491e9953a7dab16fc3cc1ca468759deb2f797ed5
Matias Cappato <71288132+mcappato@users.noreply.github.com>
2026-09-01 12:57:39 -0300

    feat(DS-6320): replace launch VIP feature with regular price

--- components/digital-trends/pre/digital-trends/vip-features.php
+++ components/digital-trends/pre/digital-trends/vip-features.php
@@ -1,23 +1,12 @@
   <!-- Features -->
-  <div class="features features--launch-price hidden--vip">
-    <div class="features__launch-price emms__fade-in">
-      <div class="features__title">
-        <h2>Precio de lanzamiento: USD <?= VIP_PRICE_CURRENT ?> sólo hasta el <?= VIP_OFFER_DEADLINE_LABEL ?>. ¡Luego vuelve a USD <?= VIP_PRICE_LIST ?>!</h2>
-      </div>
-      <div class="features__counter">
-        <?php
-        $counterTitle = '';
-        include($_SERVER['DOCUMENT_ROOT'] . '/components/date-counter.php');
-        unset($counterTitle);
-        ?>
-      </div>
-    </div>
+  <div class="features hidden--vip">
     <div class="features__item emms__fade-in features__item--reverse">
       <div class="emms__container--lg">
         <div class="features__item__image">
           <img src="src/img/pasevip.png" alt="Imagen pase VIP">
         </div>
         <div class="features__item__text">
+          <h3>Invierte en tu futuro por sólo <?= VIP_PRICE_CURRENT ?> USD</h3>
           <p><strong>Accede a:</strong></p>
           <ul class="features__item__text__list">
             <li>Más de 10 Workshops en salas reducidas.</li>
@@ -27,10 +16,8 @@
           <?php [$vipUnits, $vipCents] = vipPriceUnits(VIP_PRICE_CURRENT); ?>
           <p class="features__item__text__price features__item__text__price--live">
             <em>¡INVIERTE EN TU FUTURO POR SOLO</em><small>USD</small><?= $vipUnits ?><small>,<?= $vipCents ?>!</small>
-            <span class="features__item__text__price__before">Antes: <s>USD<?= VIP_PRICE_LIST ?></s></span>
           </p>
-          <a href="#entradas" class="emms__cta emms__cta--terciary"> ASEGURA TU PRECIO DE LANZAMIENTO
-          </a>
+          <a href="#entradas" class="emms__cta emms__cta--terciary">COMPRA TU ENTRADA AHORA</a>
         </div>
       </div>
     </div>

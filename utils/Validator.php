c05f7d072771a41f72c615ffd34fc6eb4d59969f
Matias Cappato <71288132+mcappato@users.noreply.github.com>
2026-09-03 12:06:40 -0300

    refactor: centralize email syntax validation

--- utils/Validator.php
+++ utils/Validator.php
@@ -8,9 +8,6 @@ class Validator {
         if(empty($value)) {
             throw new Exception('Validator: Error Field Required Email');
         }
-        elseif(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception('Validator: Error Field Email Invalid');
-        }
 
         return EmailAddressValidator::assertValid($value);
     }
